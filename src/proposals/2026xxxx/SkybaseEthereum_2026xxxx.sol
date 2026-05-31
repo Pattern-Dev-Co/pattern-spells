c193884e4476f1de53585c5c0343530069670069
Filip <filip@nebulaexchange.xyz>
2026-05-31 17:44:02 +0100

    feat: refactor pattern  spell into 2026-XX-XX skybase spell

---
+++ src/proposals/2026xxxx/SkybaseEthereum_2026xxxx.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity 0.8.25;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import { Ethereum } from "skybase-address-registry/Ethereum.sol";
+
+import { SkybasePayloadEthereum } from "src/libraries/SkybasePayloadEthereum.sol";
+
+/**
+ * @title   June 18, 2026 Skybase Ethereum Proposal
+ * @notice  Transfer excess USDS from the Skybase proxy.
+ * @author  Soter Labs
+ */
+contract SkybaseEthereum_2026xxxx is SkybasePayloadEthereum {
+
+    /// @dev Set recipient and amount before deployment.
+    address public constant SKYBASE_FOUNDATION_MULTISIG     = 0x1111111111111111111111111111111111111111;
+    uint256 public constant USDS_TRANSFER_AMOUNT = 100_000e18;
+
+    function _execute() internal override {
+        IERC20(Ethereum.USDS).transfer(SKYBASE_FOUNDATION_MULTISIG, USDS_TRANSFER_AMOUNT);
+    }
+}
---
+++ src/proposals/2026xxxx/SkybaseEthereum_2026xxxx.t.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.25;
+
+import { Test } from "forge-std/Test.sol";
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import { Ethereum } from "skybase-address-registry/Ethereum.sol";
+
+import { IStarGuardLike } from "src/interfaces/Interfaces.sol";
+
+import { SkybaseEthereum_2026xxxx } from "./SkybaseEthereum_2026xxxx.sol";
+
+contract SkybaseEthereum_2026xxxxTest is Test {
+
+    SkybaseEthereum_2026xxxx internal spell;
+
+    function setUp() public {
+        vm.createSelectFork(vm.rpcUrl("mainnet"), 24_887_533);
+
+        spell = new SkybaseEthereum_2026xxxx();
+
+        deal(Ethereum.USDS, Ethereum.SKYBASE_PROXY, spell.USDS_TRANSFER_AMOUNT());
+    }
+
+    function test_usdsTransfer() public {
+        address recipient = spell.SKYBASE_FOUNDATION_MULTISIG();
+        uint256 amount      = spell.USDS_TRANSFER_AMOUNT();
+
+        uint256 recipientBalanceBefore = IERC20(Ethereum.USDS).balanceOf(recipient);
+        uint256 proxyBalanceBefore     = IERC20(Ethereum.USDS).balanceOf(Ethereum.SKYBASE_PROXY);
+
+        bytes32 bytecodeHash = address(spell).codehash;
+
+        vm.prank(Ethereum.PAUSE_PROXY);
+        IStarGuardLike(Ethereum.SKYBASE_STAR_GUARD).plot({
+            addr_ : address(spell),
+            tag_  : bytecodeHash
+        });
+
+        address executed = IStarGuardLike(Ethereum.SKYBASE_STAR_GUARD).exec();
+        require(executed == address(spell), "FAILED TO EXECUTE PAYLOAD");
+
+        assertEq(
+            IERC20(Ethereum.USDS).balanceOf(recipient),
+            recipientBalanceBefore + amount,
+            "incorrect-recipient-usds-balance"
+        );
+        assertEq(
+            IERC20(Ethereum.USDS).balanceOf(Ethereum.SKYBASE_PROXY),
+            proxyBalanceBefore - amount,
+            "incorrect-proxy-usds-balance"
+        );
+    }
+}
